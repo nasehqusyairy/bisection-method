c1c8e763ea9097772c3b0115fe940fc28acd45ff
nasehqusyairy <nasyikhqusyairy@gmail.com>
2022-09-18 09:01:06 +0700

    Update NapsonErrTableModel.java

--- src/bisectionmethod/NapsonErrTableModel.java
+++ src/bisectionmethod/NapsonErrTableModel.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bisectionmethod;
 
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Nasyikh Al-Qusyairy
- */
 public class NapsonErrTableModel extends DefaultTableModel {
    
     public NapsonErrTableModel(Object[][] data,String[] header) {
